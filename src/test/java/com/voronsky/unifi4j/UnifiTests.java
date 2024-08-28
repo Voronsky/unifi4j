5790e115e59d3b9a5164afee8465e2d30c7659a8
Voronsky <roflocopter911@gmail.com>
2024-08-28 09:57:36 -0400

    new test case, WIP

--- src/test/java/com/voronsky/unifi4j/UnifiTests.java
+++ src/test/java/com/voronsky/unifi4j/UnifiTests.java
@@ -1,9 +1,13 @@
 package com.voronsky.unifi4j;
 
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.ResponseEntity;
 
-import java.util.Objects;
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class UnifiTests {
 
@@ -19,5 +23,27 @@ public class UnifiTests {
         assert response.getStatusCode().is2xxSuccessful();
         assert !response.getBody().isEmpty();
 
+        try{
+            JSONObject jsonObject = new JSONObject(response.getBody());
+            System.out.println(jsonObject);
+        } catch (JSONException err){
+            System.out.println(err.toString());
+        }
+
+    }
+
+    @Test
+    public void GetHardwaresTest(){
+        List<String> response = new ArrayList<>();
+        System.out.println("UNIFI_KEY: "+System.getenv("UNIFI_KEY"));
+        Unifi unifi = new Unifi("https://api.ui.com/ea", System.getenv("UNIFI_KEY"));
+        try {
+            response = unifi.getHardware();
+        }
+        catch(Exception e){
+            System.out.println("Exception: "+e);
+            assert false;
+        }
+        System.out.println(response);
     }
 }
