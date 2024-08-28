88776bca711d8248fc07b537424493d46f45a12c
Voronsky <roflocopter911@gmail.com>
2024-08-28 09:57:14 -0400

    additional records needed

--- src/main/java/com/voronsky/unifi4j/ReportedState.java
+++ src/main/java/com/voronsky/unifi4j/ReportedState.java
@@ -1,8 +1,6 @@
 package com.voronsky.unifi4j;
+
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-public record Data(String data, Long httpStatusCode, String traceId) {
-
-}
+public record ReportedState(String hostname, Hardware hardware, String ip, String Version, String state) {}
