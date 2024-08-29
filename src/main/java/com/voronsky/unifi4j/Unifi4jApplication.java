f09a0ec77cd4d5a1143d087c5196033a7b49e3ab
Voronsky <roflocopter911@gmail.com>
2024-08-28 23:54:35 -0400

    Setup file

---
+++ src/main/java/com/voronsky/unifi4j/Unifi4jApplication.java
@@ -0,0 +1,13 @@
+package com.voronsky.unifi4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Unifi4jApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Unifi4jApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/voronsky/unifi4j/Unifi4jApplicationTests.java
@@ -0,0 +1,13 @@
+package com.voronsky.unifi4j;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Unifi4jApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
