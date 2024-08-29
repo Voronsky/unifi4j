e95547660edaacb0f28dbbc2c401487deb64de42
Voronsky <roflocopter911@gmail.com>
2024-08-28 23:48:42 -0400

    Refactored Data record class

--- src/main/java/com/voronsky/unifi4j/Data.java
+++ src/main/java/com/voronsky/unifi4j/Data.java
@@ -3,6 +3,4 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-public record Data(String data, Long httpStatusCode, String traceId) {
-
-}
+public record Data(String hardwareId, String id, String ipAddress, ReportedState reportedState) {}
